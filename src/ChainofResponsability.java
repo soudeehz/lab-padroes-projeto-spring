65485e4db87720398745ca351a027a757fe34907
Denzel Jhones <denzelxd09@icloud.com>
2024-07-23 21:56:16 -0300

    Add Chain of Resp

---
+++ src/ChainofResponsability.java
@@ -0,0 +1,6 @@
+public class ChainofResponsability {
+
+
+    // O Chain of Responsibility é um padrão de projeto comportamental que permite que você passe pedidos por uma corrente de handlers. Ao receber um pedido, cada handler decide se processa o pedido ou o passa adiante para o próximo handler na corrente.
+    
+}
